facdf9a2ca555ada453f58d088a523f0348c909b
florent.lartet <florent.lartet@univ-tlse2.fr>
2024-10-31 12:03:37 +0100

    une config par serveur

--- archive-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('URL', 'https://iris-exams-2023.univ-tlse2.fr/webservice/rest/server.php');
-define('WSTOKEN', '6907cd4d3d2d092347606800f03b18f2');
-define('ARCHIVE_DIRNAME', "/var/www/moodledata/archive-quinquennale");
-
-?>
